597d9725011ef96655221a68bcab55117e2ac711
rogeriocaldeira <rogeriocaldeirasilva@gmail.com>
2024-02-09 02:06:25 -0300

    projeto inicial

---
+++ src/br/com/alura/minhasmusicas/modelos/Musica.java
@@ -0,0 +1,33 @@
+package br.com.alura.minhasmusicas.modelos;
+
+import javax.sound.sampled.AudioFileFormat;
+
+public class Musica extends Audio {
+    private String cantor;
+    private String album;
+    private String genero;
+
+    public String getCantor() {
+        return cantor;
+    }
+
+    public void setCantor(String cantor) {
+        this.cantor = cantor;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
